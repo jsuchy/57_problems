1fb767cc062f9ebffbcc3fbca2c424fc0089271d
Jim Suchy <jsuchy@gmail.com>
2015-08-15 12:34:34 +0100

    Felipe gets a special greeting.

--- problem_1/lib/problem_1.ex
+++ problem_1/lib/problem_1.ex
@@ -7,11 +7,28 @@ defmodule Problem_1 do
     IO.puts greeting(name)
   end
 
-  defp greeting(name) do
-    "Guten Tag, " <> name
-  end
-
   defp who do
     IO.gets "What is your name? "
+  end
+
+  defp greeting(name) do
+    greeting_for(name) <> ", " <> name
+  end
+
+  defp greeting_for(name) do
+    case name do
+      "Felipe" ->
+        german_greeting
+      _ ->
+        english_greeting
+    end
+  end
+
+  defp english_greeting do
+    "Hello"
+  end
+
+  defp german_greeting do
+    "Guten Tag"
   end
 end
--- problem_1/test/problem_1_test.exs
+++ problem_1/test/problem_1_test.exs
@@ -3,10 +3,9 @@ defmodule Problem_1Test do
   import ExUnit.CaptureIO
 
   test "greets the user" do
-    greeter = fn ->
-      Problem_1.greet
-    end
+    greeter = fn -> Problem_1.greet end
 
-    assert capture_io("Human", greeter) == "What is your name? Guten Tag, Human\n"
+    assert capture_io("Human", greeter) == "What is your name? Hello, Human\n"
+    assert capture_io("Felipe", greeter) == "What is your name? Guten Tag, Felipe\n"
   end
 end
